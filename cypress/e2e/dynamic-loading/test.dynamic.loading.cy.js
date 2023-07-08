1b14fb64660646e0b08f742a50ac535574bbf368
Elder Maicon Henrich Cordeiro <elder_maicon@hotmail.com>
2023-07-08 09:49:48 -0300

    test: including testing and tuning in the code

---
+++ cypress/e2e/dynamic-loading/test.dynamic.loading.cy.js
@@ -0,0 +1,19 @@
+describe('screen element loading test', () => {
+
+    beforeEach(() => {
+        cy.visit('https://the-internet.herokuapp.com/dynamic_loading/2');
+    })
+
+    it('screen element validation test waiting for loading', () => {
+        cy.contains('h4', 'Element rendered after the fact').should('be.visible');
+        cy.contains('button', 'Start').click();
+        cy.wait(7000);
+        cy.contains('h4', 'Hello World').should('be.visible');
+    })
+
+    it('screen element validation test waiting for loading -2', () => {
+        cy.contains('h4', 'Element rendered after the fact').should('be.visible');
+        cy.contains('button', 'Start').click();
+        cy.contains('h4', 'Hello World').should('be.visible');
+    })
+})
