21730bb537b9d2a3bb7a577daf0ee6edddc3031b
Elder Maicon Henrich Cordeiro <elder_maicon@hotmail.com>
2023-07-09 12:52:01 -0300

    refactor: login validation tests

--- cypress/e2e/form-authentication/tests.authentication.cy.js
+++ cypress/e2e/form-authentication/tests.authentication.cy.js
@@ -1,30 +1,21 @@
 describe('Login Validation Test Suite', () => {
-  
+
   beforeEach(() => {
-    cy.visit('https://the-internet.herokuapp.com/login');
+    cy.visit(Cypress.env('urlLogin'));
   })
 
   it('login validation with valid data', () => {
-    cy.contains('h2', 'Login Page').should('be.visible');
-    cy.get('#username').type('tomsmith');
-    cy.get('#password').type('SuperSecretPassword!');
-    cy.contains('button', 'Login').click();
+    cy.Login(Cypress.env('corretUser'), Cypress.env('corretPassword'));
     cy.contains('#flash', 'You logged into a secure area!').should('be.visible');
   })
 
   it('login failure validation with invalid user', () => {
-    cy.contains('h2', 'Login Page').should('be.visible');
-    cy.get('#username').type('usertest');
-    cy.get('#password').type('SuperSecretPassword!');
-    cy.contains('button', 'Login').click();
+    cy.Login(Cypress.env('errorUser'), Cypress.env('corretPassword'))
     cy.contains('#flash', 'Your username is invalid!').should('be.visible');
   })
 
-  it('login failure validation with invalid password', ()=> {
-    cy.contains('h2', 'Login Page').should('be.visible');
-    cy.get('#username').type('tomsmith');
-    cy.get('#password').type('senha123');
-    cy.contains('button', 'Login').click();
+  it('login failure validation with invalid password', () => {
+    cy.Login(Cypress.env('corretUser'), Cypress.env('errorPassword'))
     cy.contains('#flash', 'Your password is invalid!').should('be.visible');
   })
 
